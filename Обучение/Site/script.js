51da5d126b1f9cba62e57e098e8f735c04a7ed23
xonika9 <xonika9@gmail.com>
2020-05-27 21:26:23 +0800

    Pervoe zadanie+2logs

--- Обучение/Site/script.js
+++ Обучение/Site/script.js
@@ -21,6 +21,7 @@ appData.expenses[a3] = a4;
 alert(appData.budget / 30);
 
 console.log('Ваш бюджет', appData.budget);
+console.log('Ваш бюджет на день', appData.budget / 30);
 console.log(a1, '=', a2);
 console.log(a3, '=', a4);
 
