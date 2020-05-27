698d031131991c89db5b0746781b18a8682daa1c
xonika9 <xonika9@gmail.com>
2020-05-27 21:05:16 +0800

    Pervoe zadanie

---
+++ Обучение/Site/script.js
@@ -0,0 +1,21 @@
+let money = prompt("Vash byudzhet na mesyac?", ''),
+    time = prompt("Vvedite datu v formate YYYY-MM-DD", '');
+
+let appData = {
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time,
+    savings: false
+};
+
+let a1 = prompt("Vvedite obyazatel'nuyu stat'yu raskhodov v etom mesyace", ''),
+	a2 = prompt("Vo skol'ko obojdetsya?", ''),
+	a3 = prompt("Vvedite obyazatel'nuyu stat'yu raskhodov v etom mesyace", ''),
+	a4 = prompt("Vo skol'ko obojdetsya?", '');
+
+appData.expenses[a1] = a2;
+appData.expenses[a3] = a4;
+
+alert(appData.budget / 30);
